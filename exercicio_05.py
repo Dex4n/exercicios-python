d6867c5e30a897c10b165c6410694fa0e947418e
Dex4n <alexandre_marino@outlook.com>
2020-03-13 10:25:31 -0300

    Exercício 05

--- exercicio_05.py
+++ exercicio_05.py
@@ -4,14 +4,7 @@ number = int(input("Digite um número: \n"))
 
 if (number >= 0):
     print("O número (%d) digitado é positivo!" %(number))
-    if (number % 2 == 0):
-        print("O número (%d) é par!" % (number))
-    else:
-        print("O número (%d) é ímpar!" % (number))
+
 
 elif (number < 0):
     print("O número (%d) digitado é negativo!" %(number))
-    if (number % 2 == 0):
-        print("O número (%d) é par!" % (number))
-    else:
-        print("O número (%d) é ímpar!" % (number))
