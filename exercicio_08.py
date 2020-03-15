22757490b70018052a224f406b2e96769e4b56e1
Dex4n <alexandre_marino@outlook.com>
2020-03-14 21:10:45 -0300

    Exercício 08

--- exercicio_08.py
+++ exercicio_08.py
@@ -1,8 +1,9 @@
 #8 - faça um método que mostre 10 vezes na tela a palatra python (for i in range(10))
 
+def mostrar_10_vezes(p_nome):
+    for i in range(10):
+        print(p_nome)
+
 nome = 'Python'
 
-for i in range(10):
-    print(nome)
-
-
+mostrar_10_vezes(nome)
