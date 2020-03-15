ffe4098f0f5e010ce466cad38abb7bc78e10e7fb
Dex4n <alexandre_marino@outlook.com>
2020-03-14 21:22:17 -0300

    Exercício 03

--- exercicio_03.py
+++ exercicio_03.py
@@ -1,7 +1,5 @@
 #3 - Crie uma classe calculadora com as quatro operações básicas (soma, subtração, multiplicação e divisão).
-
 # usuário deve informar dois números e o programa deve fazer as quatro operações.
-
 
 class Calculadora:
     def soma (self, numero1, numero2):
@@ -16,14 +14,12 @@ class Calculadora:
     def divisao(self, numero1, numero2):
         return numero1 / numero2
 
-
 c1 = Calculadora()
 
 soma = c1.soma(1,3)
 divisao = c1.divisao(10,2)
 multiplicacao = c1.multiplicacao(2,4)
 subtracao = c1.subtracao(33,3)
-
 
 print ("Resultado da soma: %.2f" %(soma))
 print ("Resultado da divisão: %.2f" %(divisao))
